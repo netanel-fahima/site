a0d69cbcba49a7ac34f982e2dece64d08c4791c5
Netanel <netanel.fahima@gmail.com>
2021-06-03 12:29:49 +0300

    fix

---
+++ src/app/core/utils/auto-unsub.ts
@@ -0,0 +1,12 @@
+export const AutoUnsub = () => constructor => {
+  const orig = constructor.prototype.ngOnDestroy;
+  constructor.prototype.ngOnDestroy = function() {
+    for (const prop in this) {
+      const property = this[prop];
+      if (typeof property.subscribe === 'function') {
+        property.unsubscribe();
+      }
+    }
+    orig.apply();
+  };
+};
